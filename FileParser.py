647d3b4f4adf4df531c73b39880cf2fd17f3adb7
Gupta, Rohan <rohan-gupta-1@uiowa.edu>
2019-02-26 12:05:22 -0600

    Create FileParser.py
    A file filled with helper functions for readFiles()

---
+++ FileParser.py
@@ -0,0 +1,6 @@
+#FileParser.py
+#This is going to be a python file with all functions needed to parse a .txt
+#file and .csv file
+#Get the data:  title, url, and tags.
+#These can be viewed as helper functions for readFiles()
+##############################################################################
