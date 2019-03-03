e0d2820d17d74dddb302c6119880d8feb038cfa2
Gupta, Rohan <rohan-gupta-1@uiowa.edu>
2019-03-03 17:08:56 -0600

    Create WebScraper.py

---
+++ WebScraper.py
@@ -0,0 +1,14 @@
+#WebScraper.py
+#So essentially the whole point of the branch called WebScraper is to
+#basically have a continous adding function.
+#So the user would run the main.py script, and well if they got lazy and didn't
+#want to supply a .txt file or type in stuff the program would just essentailly
+#search the internet by itself and webscrape the urls it would visit, and
+#then by itself add those videos into the database
+#Essentially they would just type what they want, like "Star Wars" or "SWTOR"
+#or whatever they want and then specify how many entries they wanted.
+#If they didn't specify the default would be 10.
+
+#Programming wise it would use search() from googles interface, and essentially
+#look for a title and the url, because that's all I would need.
+#############################################################################
